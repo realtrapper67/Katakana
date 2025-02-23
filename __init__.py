e022698b390655a66f08db70d8651236ceb5831e
realtrapper67 <headway-rake-civil@duck.com>
2025-02-23 12:36:07 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# katakana/__init__.py
+
+__title__ = 'katakana'
+__author__ = 'law'
+__license__ = 'GNU | General Public License V3'
+__version__ = '0.1.0'  
+
+from .core.loader import *
+from .modules.discord import *
+from .modules.carrd import *
+from .modules.pypi import *
